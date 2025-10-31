461f671811a59d5337c6fce3462e678003d80c32
Jader Ribeiro <jader920@gmail.com>
2025-10-31 19:00:43 -0300

    cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient()
@@ -6,6 +7,7 @@ const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const users = []
 
